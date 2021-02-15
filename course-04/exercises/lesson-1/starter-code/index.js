6744e9d75624623eefbe0c08b7f73e1dab518a1e
Carsten Langer <carsten.langer@ista.com>
2021-02-15 14:21:22 +0100

    implements excersice 1

--- course-04/exercises/lesson-1/starter-code/index.js
+++ course-04/exercises/lesson-1/starter-code/index.js
@@ -15,7 +15,9 @@ exports.handler = async (event) => {
   let requestWasSuccessful
 
   const startTime = timeInMs()
-  await axios.get(url)
+  let res = await axios.get(url)
+  endTime = timeInMs()
+  let succ = res.status <= 299 ? 1: 0
 
   // Example of how to write a single data point
   // await cloudwatch.putMetricData({
@@ -37,6 +39,33 @@ exports.handler = async (event) => {
 
   // TODO: Record time it took to get a response
   // TODO: Record if a response was successful or not
+  await cloudwatch.putMetricData({
+    MetricData: [
+      {
+        MetricName: 'Latency', // Use different metric names for different values, e.g. 'Latency' and 'Successful'
+        Dimensions: [
+          {
+            Name: 'ServiceName',
+            Value: serviceName
+          }
+        ],
+        Unit: 'Milliseconds', // 'Count' or 'Milliseconds'
+        Value: endTime - startTime // Total value
+      },
+      {
+        MetricName: 'Successful', // Use different metric names for different values, e.g. 'Latency' and 'Successful'
+        Dimensions: [
+          {
+            Name: 'ServiceName',
+            Value: serviceName
+          }
+        ],
+        Unit: 'Count', // 'Count' or 'Milliseconds'
+        Value: succ // Total value
+      }
+    ],
+    Namespace: 'Udacity/Serveless'
+  }).promise()
 }
 
 function timeInMs() {
